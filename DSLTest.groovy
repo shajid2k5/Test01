ff7b4283fbb1ac19344eb90e4c5487b6ede9c023
shajid2k5 <152222166+shajid2k5@users.noreply.github.com>
2023-12-29 16:45:48 +0530

    Update DSLTest.groovy
    New for statement script

--- DSLTest.groovy
+++ DSLTest.groovy
@@ -1,8 +1,15 @@
-
-
 job('example1') {
-  steps {
-    shell('echo Hello World!')
-  }
-}
+	steps {
+shell(
+#!/bin/bash
 
+for a in 1 2 3 4 5 6 7 8 9 10
+do
+    if [$a == 5];
+    then
+        sleep 10
+       fi
+    echo "Iteration no $a"
+done)
+}
+}
