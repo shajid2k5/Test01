1419f8b4ac273d5962981924180accd76d773209
shajid2k5 <152222166+shajid2k5@users.noreply.github.com>
2023-12-29 16:32:54 +0530

    Update DSLTest.groovy

--- DSLTest.groovy
+++ DSLTest.groovy
@@ -1,13 +1,9 @@
 job('example1') {
 	steps {
-	#!/bin/bash
-for a in 1 2 3 4 5 6 7 8 9 10
+max=10
+for i in {2..$max}
 do
-    if [$a == 5];
-    then
-        sleep 10
-       fi
-    echo "Iteration no $a"
+    echo "$i"
 done
 }
 }
